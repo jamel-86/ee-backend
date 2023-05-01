c28c5de16e29a3c8141448a33ef55e0e5b10a8db
jamel-86 <jamel.nacef@gmail.com>
2023-05-01 14:47:11 +0700

    ,

--- api/projects.js
+++ api/projects.js
@@ -1,8 +1,10 @@
-// projects.js
-
+// projects.js is a router that handles all requests to /api/projects
 const express = require("express");
 const { createClient } = require("@supabase/supabase-js");
-
+// require path
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config({ path: "../.env" });
 const projectsRouter = express.Router();
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
@@ -56,13 +58,14 @@ projectsRouter.post("/", async (req, res) => {
   }
 });
 
-// Get all projects by date order
+// Get all projects by date order (only for admin, normal users can't access all projects)
 projectsRouter.get("/", async (req, res) => {
+  const { orderAsc } = req.query;
   try {
     const { data, error } = await supabase
       .from("projects")
       .select("*")
-      .order("created_at", { ascending: false });
+      .order("created_at", { ascending: orderAsc === "true" });
 
     if (error) throw error;
 
@@ -70,6 +73,74 @@ projectsRouter.get("/", async (req, res) => {
   } catch (error) {
     console.error("Failed to get projects", error);
     res.status(500).json({ error: "Failed to get projects" });
+  }
+});
+
+// get all projects that belong to a user
+projectsRouter.get("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  const { orderAsc } = req.query;
+  try {
+    const { data, error } = await supabase
+      .from("projects")
+      .select("*")
+      .eq("created_by", id)
+      .order("created_at", { ascending: orderAsc === "true" });
+    if (error) throw error;
+    res.json(data);
+  } catch (error) {
+    console.error("Failed to get projects", error);
+    res.status(500).json({ error: "Failed to get projects" });
+  }
+});
+
+// Get a project by id
+projectsRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { data, error } = await supabase
+      .from("projects")
+      .select("*")
+      .eq("id", id)
+      .single();
+    if (error) throw error;
+    res.json(data);
+  } catch (error) {
+    console.error("Failed to get project", error);
+    res.status(500).json({ error: "Failed to get project" });
+  }
+});
+
+// Update a project by id
+projectsRouter.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, description, timezone, location, image } = req.body;
+  try {
+    const { data, error } = await supabase
+      .from("projects")
+      .update({ name, description, timezone, location, image })
+      .eq("id", id);
+    if (error) throw error;
+    res.json(data);
+  } catch (error) {
+    console.error("Failed to update project", error);
+    res.status(500).json({ error: "Failed to update project" });
+  }
+});
+
+// Delete a project by id
+projectsRouter.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { data, error } = await supabase
+      .from("projects")
+      .delete()
+      .eq("id", id);
+    if (error) throw error;
+    res.json(data);
+  } catch (error) {
+    console.error("Failed to delete project", error);
+    res.status(500).json({ error: "Failed to delete project" });
   }
 });
 
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const authRouter = require("./auth");
 const app = express();
-const projectsRouter = require("./projects");
+const projectsRouter = require("./api/projects");
 const port = process.env.PORT || 3004;
 
 app.use(bodyParser.json());
 app.use("/api/auth", authRouter);
 app.use("/api/projects", projectsRouter);
+app.use("/api/projects/:id", projectsRouter);
+app.use("/api/projects/user/:id", projectsRouter);
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
